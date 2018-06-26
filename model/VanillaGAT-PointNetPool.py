62147f7e622aa323a409210150bba3a74cd92e0a
Jean-Baptiste Weibel <jb.weibel@gmail.com>
2018-06-26 19:23:51 +0200

    implement VanillaGAT-PointNetPool arch

---
+++ model/VanillaGAT-PointNetPool.py
@@ -0,0 +1,152 @@
+from logger import TimeScope
+import numpy as np
+import tensorflow as tf
+from utils.tf import fc, fc_bn, define_scope
+from utils.params import params as p
+
+from layers import attn_head, g_k
+
+MODEL_NAME = "VanillaGAT-PointNetPool"
+
+# Dropout prob params
+p.define("attn_drop_prob", 0.0)
+p.define("feat_drop_prob", 0.0)
+p.define("pool_drop_prob", 0.5)
+# Model arch params
+p.define("residual", False)
+p.define("n_heads", [8, 1])
+p.define("attn_hid_units", [16])
+p.define("pool_hid_units", [64, 64, 128, 1024])
+p.define("red_hid_units", [256, 64])
+
+
+class Model(object):
+    def __init__(self,
+                 bn_decay=None):
+        # --- I/O Tensors -----------------------------------------------------
+        with TimeScope(MODEL_NAME + "/placeholder_setup", debug_only=True):
+            self.bias_mat = tf.placeholder(tf.float32,
+                                           (None,
+                                            p.nodes_nb,
+                                            p.nodes_nb),
+                                           name="bias_mat")
+            self.node_feats = tf.placeholder(tf.float32,
+                                             (None,
+                                              p.nodes_nb,
+                                              p.feat_nb),
+                                             name="node_feats")
+            self.y = tf.placeholder(tf.float32,
+                                    [None, p.num_classes],
+                                    name="y")
+            self.is_training = tf.placeholder(tf.bool, name="is_training")
+            self.attn_drop = tf.placeholder(tf.float32, name="attn_drop_prob")
+            self.feat_drop = tf.placeholder(tf.float32, name="feat_drop_prob")
+            self.pool_drop = tf.placeholder(tf.float32, name="pool_drop_prob")
+            self.bn_decay = bn_decay
+
+        # --- Model properties ------------------------------------------------
+        with TimeScope(MODEL_NAME + "/prop_setup", debug_only=True):
+            self.inference
+            self.loss
+            # self.optimize
+
+    def get_feed_dict(self, x_batch, y_batch, is_training):
+        xb_node_feats = [np.array(x_i[0]) for x_i in x_batch]
+        xb_bias_mat = [np.array(x_i[1]) for x_i in x_batch]
+
+        attn_drop = p.attn_drop_prob if is_training else 0.
+        feat_drop = p.feat_drop_prob if is_training else 0.
+        pool_drop = p.pool_drop_prob if is_training else 0.
+
+        return {
+            self.node_feats: xb_node_feats,
+            self.bias_mat: xb_bias_mat,
+            self.y: y_batch,
+            self.attn_drop: attn_drop,
+            self.feat_drop: feat_drop,
+            self.pool_drop: pool_drop,
+            self.is_training: is_training
+        }
+
+    @define_scope
+    def inference(self):
+        """ This is the forward calculation from x to y """
+        # --- Features dim reduction ------------------------------------------
+        if p.red_hid_units:
+            with tf.variable_scope('feat_dim_red'):
+                feat_red = tf.layers.conv1d(self.node_feats,
+                                            p.red_hid_units[0], 1)
+                feat_red = tf.layers.conv1d(feat_red, p.red_hid_units[1], 1)
+        # --- Graph attention layers ------------------------------------------
+        attns = []
+        for _ in range(p.p.n_heads[0]):
+            attns.append(attn_head(feat_red,
+                                   bias_mat=self.bias_mat,
+                                   out_sz=p.attn_hid_units[0],
+                                   activation=tf.nn.elu,
+                                   in_drop=self.feat_drop,
+                                   coef_drop=self.attn_drop,
+                                   residual=False))
+        h_1 = tf.concat(attns, axis=-1)
+        for i in range(1, len(p.attn_hid_units)):
+            attns = []
+            for _ in range(p.n_heads[i]):
+                attns.append(attn_head(h_1,
+                                       bias_mat=self.bias_mat,
+                                       out_sz=p.attn_hid_units[i],
+                                       activation=tf.nn.elu,
+                                       in_drop=self.feat_drop,
+                                       coef_drop=self.attn_drop,
+                                       residual=p.residual))
+            h_1 = tf.concat(attns, axis=-1)
+        # out = []
+        # for i in range(p.n_heads[-1]):
+        #     out.append(attn_head(h_1,
+        #                          bias_mat=self.bias_mat,
+        #                          out_sz=p.num_classes,
+        #                          activation=lambda x: x,
+        #                          in_drop=self.feat_drop,
+        #                          coef_drop=self.attn_drop,
+        #                          residual=False))
+        # logits = tf.add_n(out) / p.n_heads[-1]
+        attn_out = h_1
+
+        # --- Set Pooling -----------------------------------------------------
+        with tf.variable_scope('graph_pool'):
+            g_pool = g_k(attn_out, 'g_' + str(0),
+                         filter_num=p.pool_hid_units[0],
+                         is_training=self.is_training,
+                         bn_decay=self.bn_decay)
+            for i in range(1, len(p.pool_hid_units)):
+                g_pool = g_k(g_pool, 'g_' + str(i),
+                             filter_num=p.pool_hid_units[i],
+                             is_training=self.is_training,
+                             bn_decay=self.bn_decay)
+
+            # Pooling of the pairs
+            max_gg = tf.reduce_max(g_pool, axis=1, name='max_g')
+            fcg = fc_bn(max_gg, 256,
+                        scope='fcg',
+                        is_training=self.is_training,
+                        bn_decay=self.bn_decay)
+
+        return fc(fcg, p.num_classes,
+                  activation_fn=None, scope='logits')
+
+    @define_scope
+    def loss(self):
+        # Cross-entropy loss
+        with tf.variable_scope('cross_entropy'):
+            diff = tf.nn.softmax_cross_entropy_with_logits(
+                    labels=self.y,
+                    logits=self.inference)
+
+            cross_entropy = tf.reduce_mean(diff)
+        tf.summary.scalar('cross_entropy_avg', cross_entropy)
+
+        reg_loss = tf.losses.get_regularization_loss()
+        tf.summary.scalar('regularization_loss_avg', reg_loss)
+
+        total_loss = cross_entropy + reg_loss
+        tf.summary.scalar('total_loss', total_loss)
+        return total_loss
--- model/layers.py
+++ model/layers.py
@@ -1,9 +1,12 @@
 import tensorflow as tf
+from utils.tf import weight_variable, bias_variable, batch_norm_for_conv1d
 
 
-# Code from github.com/PetarV-/GAT
 def attn_head(seq, out_sz, bias_mat, activation,
               in_drop=0.0, coef_drop=0.0, residual=False):
+    """
+    Layer originally from github.com/PetarV-/GAT
+    """
     with tf.name_scope('my_attn'):
         if in_drop != 0.0:
             seq = tf.nn.dropout(seq, 1.0 - in_drop)
@@ -37,3 +40,24 @@ def attn_head(seq, out_sz, bias_mat, activation,
                 seq_fts = ret + seq
 
         return activation(ret)  # activation
+
+
+def g_k(tens_in, scope, filter_num, is_training, bn_decay):
+    feat_num = tens_in.get_shape()[-1].value
+    filter_shape = [1, feat_num, filter_num]
+    with tf.variable_scope(scope):
+        kernel = weight_variable(filter_shape)
+        biases = bias_variable([filter_num])
+        g_k = tf.nn.conv1d(tens_in, kernel, stride=1,
+                           padding='SAME') + biases
+        with tf.variable_scope("max_var"):
+            max_var = weight_variable([filter_num])
+        g_k = g_k - tf.expand_dims(max_var*tf.reduce_max(g_k, axis=1,
+                                                         name='max_g'),
+                                   1)
+        g_k_norm = batch_norm_for_conv1d(
+                g_k,
+                is_training=is_training,
+                bn_decay=bn_decay,
+                scope='bn')
+        return tf.nn.relu(g_k_norm)
